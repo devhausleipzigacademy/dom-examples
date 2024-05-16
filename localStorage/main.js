b1f69a8316029e4a05b6482045b8c5eab1ec95bd
Julian <birdman.coding.1991@gmail.com>
2024-05-16 12:00:44 +0200

    add deadline

---
+++ localStorage/main.js
@@ -0,0 +1,3 @@
+localStorage.setItem("myCat", "Tom");
+localStorage.setItem("catAge", 4);
+localStorage.setItem("catAlive", true);
--- todo/todo.js
+++ todo/todo.js
@@ -1,25 +1,40 @@
 // 1. get elements by ID
 const todoInput = document.getElementById('todoInput'); // 1.1 todoInput
-const taskButton = document.getElementById('taskButton'); // 1.2 taskButton
+const todoForm = document.getElementById('todoForm'); // 1.2 todoForm
 const todoList = document.getElementById('todoList'); // 1.3 todoList
+const deadline = document.getElementById('deadline');
+
+// create ID tracker
+let id = localStorage.getItem("id");
+
+if (id === null) {
+    id = 0;
+    localStorage.setItem("id", id);
+}
+
 
 // 2. create Event Listener for adding tasks
-function addTask() {
+function addTask(event) {
+    event.preventDefault();
     const newItem = todoInput.value.trim();
-    if (newItem === "") {
-        alert("Empty item...");
+    const newDeadline = deadline.value;
+    if (newItem === "" || newDeadline === "") {
+        alert("Incomplete form...");
         return;
     }
     const newTask = document.createElement('tr');
     newTask.innerHTML = `
     <td><input type="checkbox" class="form-check-input mt-0"></td>
     <td><span>${newItem}</span></td>
+    <td><date>${newDeadline}</date></td>
     <td><button class="removeBtn btn btn-danger">Remove</button></td>`;
     todoList.appendChild(newTask);
     todoInput.value = '';
+    id++;
+    localStorage.setItem("id", id);
 }
 
-taskButton.addEventListener('click', addTask);
+todoForm.addEventListener('submit', addTask);
 
 todoInput.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
